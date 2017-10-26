b54bf72ed43905984ba8238b91a115157e101f8d
Maxenc3 <maxence.coupet@gmail.com>
2017-10-26 21:19:52 +0200

    order

--- projet/order_book_analysis.py
+++ projet/order_book_analysis.py
@@ -62,7 +62,7 @@ def order_book_analysis(exchange, market):
                     bid_walls.append(orders[i]['price'])
     return bid_walls, ask_walls
 
-print(order_book_analysis("BTER", "BTC/CNY"))
+#print(order_book_analysis("BTER", "BTC/CNY"))
         
     
 
@@ -97,7 +97,7 @@ def variation(exchange, market, volume):
     sell_moove = moove_consequences(exchange, market, volume, "SELL")[3]
     return 100*(buy_moove - sell_moove)/buy_moove
 
-print(variation("BTER", "BTC/CNY", 3))
+print(moove_consequences("LIVE", "BTC/USD", 17, "BUY"))
 
 def visualize_order_book(exchange, market):
     order_book_data = get_order_book(exchange, market)
